873a4d02c3f2d4fe0fe0a13d4aa3752e073b5d6c
Aesirteam <zhongkui@139.com>
2023-06-27 17:21:02 +0800

    fixed: topic name rule

--- gkafka/gkafka.go
+++ gkafka/gkafka.go
@@ -170,7 +170,7 @@ func (k *Kafka) Parse(e *canal.RowsEvent) ([]interface{}, error) {
 
 	// 博士之前把解析出来的binlog存放在redis,这里就先不存储了
 	if k.c.Alias == "" {
-		k.c.Alias = "__"
+		k.c.Alias = "__mysql__"
 	}
 
 	topic := fmt.Sprintf("%s.%s.%s", k.c.Alias, e.Table.Schema, e.Table.Name)
